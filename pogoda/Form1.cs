24da4db4eddeda350b7f790cae974f890e36bb94
1908391 <1908391@TE-GDA-D-D36G12>
2022-12-12 15:43:10 +0100

    Dodaj pliki projektów.

---
+++ pogoda/Form1.cs
@@ -0,0 +1,28 @@
+using System.Text.Json;
+
+namespace pogoda
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            HttpClient client = new HttpClient();
+            string dziala = client.GetStringAsync("https://api.open-meteo.com/v1/forecast?latitude=54.35&longitude=18.65&hourly=temperature_2m&current_weather=true&timezone=Europe%2FBerlin").Result;
+            JsonDocument document = JsonDocument.Parse(dziala);
+            JsonElement elemt = document.RootElement.GetProperty("current_weather");
+            label1.Text = elemt.GetProperty("temperature").ToString();
+            label2.Text = elemt.GetProperty("windspeed").ToString();
+            label3.Text = elemt.GetProperty("weathercode").ToString();
+
+
+
+
+
+        }
+    }
+}
